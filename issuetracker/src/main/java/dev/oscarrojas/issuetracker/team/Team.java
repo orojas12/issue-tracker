ff1967630fc6052dc15976457ee303ff5c4cf260
oscar <orojas12@outlook.com>
2024-04-30 17:58:11 -0600

    add getters and setters

--- issuetracker/src/main/java/dev/oscarrojas/issuetracker/team/Team.java
+++ issuetracker/src/main/java/dev/oscarrojas/issuetracker/team/Team.java
@@ -70,4 +70,36 @@ public class Team {
         }
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Set<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Set<User> members) {
+        this.members = members;
+    }
+
 }
